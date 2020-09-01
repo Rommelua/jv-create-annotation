3cbfcd0cabdaed296e673f17fe4d43fb1d2c2d8f
Leonid <rommel@ukr.net>
2020-09-01 23:00:47 +0300

    betDao initialization without hard code done

--- src/main/java/core/basesyntax/exception/AnnotationAbsentException.java
+++ src/main/java/core/basesyntax/exception/AnnotationAbsentException.java
@@ -4,4 +4,8 @@ public class AnnotationAbsentException extends RuntimeException {
     public AnnotationAbsentException(String message) {
         super(message);
     }
+
+    public AnnotationAbsentException(String message, Throwable cause) {
+        super(message, cause);
+    }
 }
--- src/main/java/core/basesyntax/factory/Factory.java
+++ src/main/java/core/basesyntax/factory/Factory.java
@@ -6,21 +6,52 @@ import core.basesyntax.dao.UserDao;
 import core.basesyntax.dao.UserDaoImpl;
 import core.basesyntax.exception.AnnotationAbsentException;
 import core.basesyntax.lib.Dao;
+import java.lang.reflect.Constructor;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
 public class Factory {
     public static final String EXCEPTION_MESSAGE = " class doesnt have Dao annotation";
+    public static final String DAO_PACKAGE = "core.basesyntax.dao.";
+    public static final Path PATH_TO_DAO_CLASSES = Path.of("target/classes/core/basesyntax/dao");
+    private static final ClassLoader classLoader = Factory.class.getClassLoader();
+
     private static BetDao betDao;
     private static UserDao userDao;
 
     public static BetDao getBetDao() {
         if (betDao == null) {
-            if (BetDaoImpl.class.isAnnotationPresent(Dao.class)) {
-                betDao = new BetDaoImpl();
-            } else {
-                throw new AnnotationAbsentException("BetDaoImpl" + EXCEPTION_MESSAGE);
-            }
+            initBetDao();
         }
         return betDao;
+    }
+
+    private static void initBetDao() {
+        try {
+            Class<?> betDaoClazz = Files.walk(PATH_TO_DAO_CLASSES)
+                    .filter((e -> e.toString().endsWith(".class")))
+                    .map(path -> DAO_PACKAGE + path.getFileName().toString()
+                            .replace(".class", ""))
+                    .map(Factory::loadClass)
+                    .filter(clazz -> clazz.isAnnotationPresent(Dao.class)
+                                     && clazz.getInterfaces()[0].getName()
+                                             .equals(BetDao.class.getName()))
+                    .findFirst()
+                    .orElseThrow(() -> new AnnotationAbsentException(
+                            "Can't find implementation for BetDao"));
+            Constructor<?> constructor = betDaoClazz.getDeclaredConstructor();
+            betDao = (BetDaoImpl) constructor.newInstance();
+        } catch (Exception e) {
+            throw new AnnotationAbsentException("Can't find implementation for BetDao", e);
+        }
+    }
+
+    private static Class<?> loadClass(String className) {
+        try {
+            return classLoader.loadClass(className);
+        } catch (ClassNotFoundException e) {
+            throw new AnnotationAbsentException("Can't find implementation for BetDao", e);
+        }
     }
 
     public static UserDao getUserDao() {
